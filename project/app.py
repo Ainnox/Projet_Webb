3394aee2e32776a7830cd90bf16332777b139b03
Theo LAUZERAL <theo.lauzeral@gmail.com>
2022-11-07 09:31:29 +0000

    init -> app

--- project/app.py
+++ project/app.py
@@ -1,6 +1,66 @@
-from flask import Flask
-app = Flask(__name__)
+import sqlite3
+from flask import Flask, session
 
-@app.route('/')
-def hello_world():
-    return 'Hello Sammy!'
+
+def get_db_connection():
+    conn = sqlite3.connect('project/database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+def get_perm(right):
+    co = get_db_connection()
+    cur = co.cursor()
+    cur.execute("SELECT rights_name FROM Users NATURAL JOIN Role NATURAL JOIN Posses NATURAL JOIN RIGHTS WHERE email=?",
+                (session.get('email'),))
+    rights = cur.fetchall()
+    co.close()
+    for r in rights:
+        print(r[0])
+        if r[0] == right:
+            return True
+    return False
+
+
+def get_role(need_role):
+    co = get_db_connection()
+    cur = co.cursor()
+    cur.execute("SELECT role_name FROM Users NATURAL JOIN Role WHERE email=?",
+                (session.get('email'),))
+    role = cur.fetchone()
+    co.close()
+    if role and role[0] == need_role:
+        return True
+    else:
+        return False
+
+
+def event_follow(id_event):
+    co = get_db_connection()
+    cur = co.cursor()
+    cur.execute("SELECT * FROM Selected WHERE email=? AND id_event=?", (session.get('email'), id_event))
+    followed = cur.fetchone()
+    co.close()
+    if followed:
+        return True
+    return False
+
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config['SECRET_KEY'] = 'maSuperClé'
+
+    # blueprint for auth routes in our app
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
+    # blueprint for non-auth parts of app
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    app.jinja_env.globals.update(get_role=get_role)
+    app.jinja_env.globals.update(get_perm=get_perm)
+    app.jinja_env.globals.update(event_follow=event_follow)
+
+    return app
